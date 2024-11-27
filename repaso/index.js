8786320f1b50ac006cb35c9360db53da01f6b213
alexprometeo <alexandru.cucuruzeanu@mercanza.es>
2024-11-27 11:30:34 +0100

    ejercicio repaso;

---
+++ repaso/index.js
@@ -0,0 +1,7 @@
+let elemento = document.getElementById("titulo");
+
+console.log("elemento titulo: ", elemento);
+
+elemento.innerHTML = "Modificado!";
+
+document.getElementById("titulo").innerHTML = "Modificado 2!";
