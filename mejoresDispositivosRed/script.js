a0d1754b11ffb80f1ba62ef560e6e4cf9c0c4f55
alexprometeo <alexandru.cucuruzeanu@mercanza.es>
2024-12-04 11:16:24 +0100

    examen_mejoresDispositivos

---
+++ mejoresDispositivosRed/script.js
@@ -0,0 +1,26 @@
+/*
+    Completar cada función para conseguir el resultado esperado de cada una.
+    
+    Recuerda utilizar console.log para ayudarte a ver si las variables van tomando los valores esperados
+*/
+
+
+// Cambiar el texto del primer dispositivo
+function cambiarTextoPrimerDispositivo() {
+    // Seleccionamos el primer elemento por su ID
+    
+    // Cambiamos el texto interno
+    
+}
+
+// Añadir un emoticono en el último dispositivo
+function añadirEmoji() {    
+    // Seleccionamos el último dispositivo dentro de la lista y se lo asignamos a una variable
+
+    // Creamos un nuevo elemento <span> y se lo asignamos a una nueva variable
+
+    // Añadimos el emoticono "🛠" dentro del nuevo elemento    
+    
+    // Añadimos el nuevo elemento recien creado al final del 
+
+}
