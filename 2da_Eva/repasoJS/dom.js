bcffa126ca45a53eb6c970e172cf67d56a2e95e1
alexprometeo <alexandru.cucuruzeanu@mercanza.es>
2025-02-05 11:42:49 +0100

    repasoJS - seleccion por clase, etiqueta, modificación de estilos y creación y eliminación de elementos;

--- 2da_Eva/repasoJS/dom.js
+++ 2da_Eva/repasoJS/dom.js
@@ -19,5 +19,29 @@ const elementos = document.getElementsByClassName("elemento");
 console.log(elementos);
 
 
-//Las colecciones, arrays y matrices, siempre comienzan por cero, al hacer referencia a los elementos es importante tenerlo en cuenta
+//Las colecciones, arrays y matrices, siempre comienzan por cero, al hacer referencia 
+// a los elementos es importante tenerlo en cuenta
 console.log("Primer elemento de la colección de elementos con la clase 'elemento'", elementos[0]);
+
+const menu = document.getElementsByTagName("ul");
+console.log(menu);
+
+//Seleccionamos el listado por su id y le modificamos el color de fondo mediante el atributo style
+const listaElementos = document.getElementById("listaElementos");
+listaElementos.style.backgroundColor = "antiquewhite";
+//listaElementos.style = "background-color: brown!important;";
+
+
+//Creamos un nuevo elemento y le asignamos valores a los atributos más relevantes, de esa manera conseguiremos asignarle su propio identificador, clase y el contenido de la etiqueta
+let elemento4 = document.createElement("li");
+console.log(elemento4);
+elemento4.id = "elemento4";
+elemento4.className = "elemento";
+elemento4.textContent = "Elemento 4";
+
+//Con el appendChild añadimos elementos html como elementos hijos a un elemento existente
+listaElementos.appendChild(elemento4);
+
+
+//Eliminar un elemento
+listaElementos.remove();
