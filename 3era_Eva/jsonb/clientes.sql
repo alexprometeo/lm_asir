0275ef1be580ce361f11a38fec3f050ecc5c8423
alexprometeo <alexandru.cucuruzeanu@mercanza.es>
2025-04-23 11:11:20 +0200

    PostgreSQL - JSONB;

---
+++ 3era_Eva/jsonb/clientes.sql
@@ -0,0 +1,26 @@
+--CLIENTES: id, nombre, empresa, ventas, contactos  
+
+
+-- CREATE TABLE Clientes (
+-- 	id SERIAL PRIMARY KEY,
+-- 	nombre VARCHAR(50) NOT NULL,
+-- 	empresa VARCHAR(250) NULL,
+-- 	ventas int NULL,
+-- 	contactos jsonb NULL
+-- );
+
+
+INSERT INTO Clientes (nombre, empresa, ventas, contactos)
+VALUES (
+	'Valve'
+	, 'Valve'
+	, 30
+	, '{
+		"nombre":"John",
+		"apellidos":"Doe",
+		"genero": "M",
+		"correo": "john.doe@mail.com",
+		"telefono": "555888777",
+		"direccion": "123 Pelham"
+    }' 
+);
