e475b08d64133c12ce2290158fced1ffc9fecdab
alexprometeo <alexandru.cucuruzeanu@mercanza.es>
2025-04-30 11:09:08 +0200

    jsonb - productos - Create e insert;

---
+++ 3era_Eva/jsonb/productos.sql
@@ -0,0 +1,47 @@
+/*
+    Tabla productos:
+     - producto VARCHAR(250)
+     - caracteristicas JSONB (color, marca, material)
+
+    Insertar 3 registros nuevos;
+*/
+
+CREATE TABLE Productos (
+	id SERIAL PRIMARY KEY,
+	producto VARCHAR(50) NOT NULL,	
+	caracteristicas jsonb NULL
+);
+
+
+INSERT INTO Productos (producto, caracteristicas)
+VALUES (
+	'Camisa'
+	, '{
+		"color":"Azul marino",
+		"marca":"H&M",
+		"material":"lino"
+    }' 
+);
+
+INSERT INTO Productos (producto, caracteristicas)
+VALUES
+(
+	'Portatil'
+	, '{
+		"color": "Negro",
+		"marca": "Asus",
+		"material": "Plastico"
+    }' 
+);
+
+INSERT INTO Productos (producto, caracteristicas)
+VALUES
+(
+	'Cuaderno'
+	, '{
+		"color":"Verde",
+		"marca":"Rubio",
+		"material":"Papel"
+    }'  
+)
+;
