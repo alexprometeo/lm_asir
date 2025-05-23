53f4ef2b18097a35535ab259af3b6ed80744c80d
alexprometeo <alexandru.cucuruzeanu@mercanza.es>
2025-05-23 09:37:39 +0200

    Python - Conversión de tipos

---
+++ 3era_Eva/python/03_cast.py
@@ -0,0 +1,19 @@
+#  Importar métodos del sistema operativo
+from os import system
+
+# Limpiar la terminal
+if system("clear") !=0: system("cls")
+
+print(2 + int("10"))
+
+print(str(2) + "10" + "asdasd")
+
+print(int(3.9999))
+
+print(round(2.5))
+
+print(bool("True"))
+print(bool(""))
+print(bool(" "))
+print(bool("False"))
+
